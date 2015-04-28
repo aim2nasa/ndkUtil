c7161b592b6c6855f6659393c465d03376a80fbc
skwak <skwak@ea6ef2fe-d6ee-1846-a563-068f6ef6c178>
2015-04-28 07:47:16 +0000

    threshold값을 디폴트로 초기화하고 입력받으면 값을 갱신하도록 구현

--- resize/jni/resize.cpp
+++ resize/jni/resize.cpp
@@ -6,19 +6,21 @@
 typedef unsigned char 	_u8;
 typedef unsigned short 	_u16;
 typedef unsigned int 	_u32;
-typedef signed char 	_s8;
-typedef signed short 	_s16;
-typedef signed int 		_s32;
+typedef signed char		_s8;
+typedef signed short	_s16;
+typedef signed int		_s32;
 
-#define BUFFER_SIZE		1024
+#define BUFFER_SIZE			1024
+#define DEFAULT_THRESHOLD	589824	//576x1024
 
 using namespace std;
 
 int main(int argc,char* argv[])
 {
 	if(argc<6) {
-		cout<<"usage: resize <Bytes per Pixel> <width> <height> <source raw file> <resized raw file>"<<endl;
+		cout<<"usage: resize <Bytes per Pixel> <width> <height> <source raw file> <resized raw file> <threshold(opt)>"<<endl;
 		cout<<"ex. resize 4 1440 2560 dump.raw resized.raw"<<endl;
+		cout<<"    threshold is calculated as width x height, default is 576x1024=589824"<<endl;
 		cout<<"Copyright© by Funzin"<<endl<<endl;
 		return -1;
 	}
@@ -26,7 +28,9 @@ int main(int argc,char* argv[])
 	int nBpp = atoi(argv[1]);
 	int nWidth = atoi(argv[2]);
 	int nHeight = atoi(argv[3]);	
-	cout<<"Bytes/Pixel:"<<nBpp<<",Width:"<<nWidth<<",Height:"<<nHeight<<endl;
+	int nThreshold = DEFAULT_THRESHOLD;
+	if(argc>6) nThreshold = atoi(argv[6]);
+	cout<<"Bytes/Pixel:"<<nBpp<<",Width:"<<nWidth<<",Height:"<<nHeight<<",Threshold:"<<nThreshold<<endl;
 	cout<<"source raw file size:"<<nBpp*nWidth*nHeight<<"bytes"<<endl;
 	
 	FILE* fp = fopen(argv[4],"rb");
