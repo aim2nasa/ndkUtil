7e18b58175460d566212f14ba588ff3e6cc2b46f
skwak <skwak@ea6ef2fe-d6ee-1846-a563-068f6ef6c178>
2015-04-28 04:52:14 +0000

    스크립트에  dump끝나면 dump파일을 host로 갖고오는 명령 추가

--- cap/all.sh
+++ cap/all.sh
@@ -6,3 +6,4 @@ ndk-build
 adb push obj/local/armeabi/cap /data/local/tmp/
 adb shell "chmod 777 /data/local/tmp/cap"
 adb shell "data/local/tmp/cap /data/local/tmp/dump.raw"
+adb pull /data/local/tmp/dump.raw .
