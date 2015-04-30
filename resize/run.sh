a829288c996d2f22f1e0efe7a3332f18bd4af4ff
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-04-30 02:09:36 +0000

    run스크립트에서 인자가 빠진것 추가

--- resize/run.sh
+++ resize/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-adb shell "/data/local/tmp/resize 4 1440 2560 /data/local/tmp/dump.raw /data/local/tmp/resized.raw 0"
+adb shell "/data/local/tmp/resize 4 1440 2560 /data/local/tmp/dump.raw /data/local/tmp/resized.raw 0 576 1024"
